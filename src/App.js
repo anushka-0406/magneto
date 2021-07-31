8ec6a444f1e5f24959cf62d30147c3ee41c0a4b2
Siddharth Subramanian <sidsbrmnn@gmail.com>
2021-08-01 00:44:53 +0530

    Update dependencis
    - Update react, react-dom and cra-template
    - Remove bootstrap as styling choice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from "react";
 
-import Form from "./components/Form";
-import Navbar from "./components/Navbar";
-import Section from "./components/Section";
+const App = () => {
+  const generateUri = (hash, name) => {
+    return `magnet:?xt=urn:btih:${hash}&dn=${encodeURIComponent(name)}`;
+  };
 
-const App = () => (
-  <>
-    <Navbar />
-    <Section>
-      <Form />
-    </Section>
-  </>
-);
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <a href="/" className="navbar-brand">
+            Mag-netti
+          </a>
+        </div>
+      </nav>
+      <section className="container py-5">
+        <div className="row">
+          <div className="col-lg-6 col-md-8 col-sm-10 col-12">
+            <form>
+              <div className="form-group">
+                <label htmlFor="hash">Info hash</label>
+                <input
+                  type="text"
+                  name="hash"
+                  id="hash"
+                  className="form-control form-control-lg"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="name">Torrent name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="form-control form-control-lg"
+                />
+              </div>
+              <a className="btn btn-primary">
+                Get <i className="fa fa-magnet" />
+              </a>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default App;
